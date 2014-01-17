aad648894c78c534ab14cdd1df5aff1fbf17f164
jpardobl <jpardo@digitalhigh.es>
2014-01-17 09:49:39 +0100

    Preparing for pypi release 0.10.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='hautomation_x10',
-    version='0.10',
+    version='0.10.1',
     packages=['hautomation_x10'],
     include_package_data=True,
     license='BSD License',
