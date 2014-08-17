acf14f91a7d6f46eba78ce85ade5cdcbdbe3bdbd
jpardobl <kit2707888>
2014-08-17 09:49:58 +0200

    Adapted the deploy command to get rid of ginsfsm. Therefore added the necessary fields deploy command needs to populate the protocol

--- hautomation_x10/deploy.py
+++ hautomation_x10/deploy.py
@@ -14,7 +14,17 @@ def populate_db():
 
         from hacore.models import Protocol
         if Protocol.objects.filter(name="X10").count() == 0:
-            Protocol(name="X10", gobj_name="driver_X10", module="hautomation_x10", validate_address_module="hautomation_x10.utils").save()
+            Protocol(
+                name="X10",
+                gobj_name="driver_X10",
+                module="hautomation_x10",
+                validate_address_module="hautomation_x10.utils",
+                switch="hautomation_x10.cmds.pl_switch",
+                bri="hautomation_x10.cmds.pl_bri",
+                dim="hautomation_x10.cmds.pl_dim",
+                all_lights_on="hautomation_x10.cmds.pl_all_lights_on",
+                all_lights_off="hautomation_x10.cmds.pl_all_lights_off").save()
+
             sys.stdout.writelines("Protocoll successfully populated into db")
         else:
             sys.stdout.writelines("Protocol is already at the db. No changes made.")
